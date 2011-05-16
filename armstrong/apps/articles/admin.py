49eb830fc025fd647ca33fc067bf0bca6d37f155
Travis Swicegood <development@domain51.com>
2011-05-16 18:46:44 -0500

    use the new style available from arm_content.admin.fieldsets

--- armstrong/apps/articles/admin.py
+++ armstrong/apps/articles/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from reversion.admin import VersionAdmin
 
+from armstrong.core.arm_content.admin import fieldsets
 from .models import Article
 
 
@@ -10,16 +11,8 @@ class ArticleAdmin(VersionAdmin):
             'fields': ('title', 'summary', 'body', ),
         }),
 
-        # TODO: Refactor this into a common utility function
-        ('Publication Information', {
-            'fields': ('pub_date', 'pub_status', 'sites', ),
-        }),
-
-        # TODO: Refactor this into a fieldset_for(some_model) method so it can
-        #       be reused in other apps.
-        ('Author Information', {
-            'fields': ('authors', 'authors_override', 'authors_extra'),
-        }),
+        fieldsets.publication(),
+        fieldsets.authors(),
     )
 
 
