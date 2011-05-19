0be6ab488b74d628229c54a42117f5e69497737b
Travis Swicegood <development@domain51.com>
2011-05-19 16:58:18 -0500

    Add support for displaying with tree sections properly

--- armstrong/apps/articles/admin.py
+++ armstrong/apps/articles/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from reversion.admin import VersionAdmin
 
 from armstrong.core.arm_content.admin import fieldsets
+from armstrong.core.arm_sections.admin import SectionTreeAdminMixin
 from .models import Article
 
 
-class ArticleAdmin(VersionAdmin):
+class ArticleAdmin(SectionTreeAdminMixin, VersionAdmin):
     fieldsets = (
         (None, {
             'fields': ('title', 'summary', 'body',
