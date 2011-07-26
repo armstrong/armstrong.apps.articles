294eca830f1252057461dec6840303fca7760da3
Dave McLain <dmclain@texastribune.org>
2011-07-26 16:25:11 -0500

    add published to Article

--- armstrong/apps/articles/models.py
+++ armstrong/apps/articles/models.py
@@ -1,6 +1,9 @@
 from armstrong.apps.content.models import Content
+from armstrong.core.arm_content.mixins.publication import PublishedManager
 from django.db import models
 
 
 class Article(Content):
     body = models.TextField()
+
+    published = PublishedManager()
