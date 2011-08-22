90a5d019980a17e7f362d4ce9a6554590f67348d
Travis Swicegood <development@domain51.com>
2011-08-22 16:20:31 -0500

    add in support for related content

--- armstrong/apps/articles/admin.py
+++ armstrong/apps/articles/admin.py
@@ -4,6 +4,7 @@ from reversion.admin import VersionAdmin
 from armstrong.core.arm_content.admin import fieldsets
 from armstrong.core.arm_sections.admin import SectionTreeAdminMixin
 from armstrong import hatband
+from armstrong.apps.related_content.admin import RelatedContentInline
 from .models import Article
 
 
@@ -17,6 +18,10 @@ class ArticleAdmin(SectionTreeAdminMixin, VersionAdmin, hatband.ModelAdmin):
         fieldsets.PUBLICATION,
         fieldsets.AUTHORS,
     )
+
+    inlines = [
+        RelatedContentInline,
+    ]
 
 
 admin.site.register(Article, ArticleAdmin)
